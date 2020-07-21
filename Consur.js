659ea218608426d3bea09aaf21a00ad205ef447d
Iyke <iducyril@gmail.com>
2020-07-21 20:02:47 +0100

    initial

---
+++ Consur.js
@@ -0,0 +1,20 @@
+var responses = {
+hello: ["hi", "how are you doing?", "How may I help you"],
+mayIKnowYou: ["I was created to improve your experience", "what do you mean?", "I'm a robot that helps one navigate through this application to improve your experience"],
+howOldAreYou: ["is that necessary?", "i wad created a longbtimr ago", "I've long existed"],
+whoIsThePresidentOfNigeria: ["Major General Muhammadu Buhari", "His Excellency, Muhammadu Buhari GCFR"],
+whereIsTheCapitalOfNigeria: ["Currently, it is sited in Abuja", "Before now, it was sited in Calabar, it was then moved to Lagos and now, Abuja"],
+whenWasTheMovieExtractionProduced: [2020, "It was produced in the year 2020"],
+whenDidTheCoronaVirusBreakOut: ["The virus according to record, broke out in the year 2019", "The virus span across the world in the year 2020"],
+whoIsYourMaster: ["I have no master", "I pledge no one my loyalty, hence, no master have I"],
+howLongDoYouWorkEachDay: ["as long as i have someone to keep me company, i would not mind working all day", "i do not have a time frame", "i work as long as i have someone to interact with"],
+howReliableAreYou: ["within my bound, I am perfectly efficient", "no one is perfect but i can give you my assurance"],
+howManyPlayersPlaySoccer: ["11 player per team", "ten players wifh a goal keeper per team which makes it a total of twenty two players on the pitch"],
+howLongDoesASoccerGameLast: ["Ninety minutes", "It last for a period of ninety minutes which an injury time or extra time of thirty minutes can be added"],
+whenDidTheNigeriaBiafraBreakOut: ["It broke out in 1966 as a result of some indifferences noticed by the easterners", "it started in 1966 and ended in 1970 with the decision that there was no Victor and no vanquish"],
+whenWasNyscEstablished: ["it was established after the Nigeria Biafra war fo foster the unity of the country", "After the Nigeria Biafra war, General Yakubu Gowon established it to incite the spirit of unity among citizens"],
+whenDidNigeriaGainedHerIndependence: [1960, "on October 1, 1960"],
+whenWasNigeriaRefferdToAsARepuicanState: [1963, "three years after gaining her freedom"],
+whatIsTheNecessaryVaccinationBeforeOneLeavesACountryForAnotherCalled: ["Before one leaves a country for another, he or she would be required to receive yellow fever vaccination", "Yellow fever vaccination"],
+
+};
